ecd9c2cc4082e56432d9c3b3d0195041fddcfee2
ainsleyk <56616943+ainsleyk@users.noreply.github.com>
2019-11-05 15:47:25 -0800

    Add files via upload

---
+++ callfiles.js
@@ -0,0 +1,12 @@
+
+ var mymap = L.map('mapid').setView([47.258, -120.465], 7);
+ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+   maxZoom: 18,
+   id: 'mapbox.light',
+   accessToken: 'pk.eyJ1IjoiYWluc2xleWttIiwiYSI6ImNrMmt1cDhnaTAwZDgzY2xrcW1zamIxNGgifQ.-0f1V1moN7hnx8mzPD7hxQ'
+ }).addTo(mymap);
+
+ $.getJSON("farmersmarkets.json",function(data){
+   L.geoJson(data).addTo(mymap);
+ });
